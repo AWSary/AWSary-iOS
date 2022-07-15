dcf3473de1aadb8b07eb7a5a6fe6cfbb7e3541ed
Tiago Rodrigues <mail@tig.pt>
2022-07-15 22:03:49 +0100

    initial commit

---
+++ awsary/Shared/awsaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  awsaryApp.swift
+//  Shared
+//
+//  Created by Tiago Rodrigues on 15/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct awsaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
