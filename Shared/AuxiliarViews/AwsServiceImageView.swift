764d9ac33e2891a6bf160c9ae27f195ad6674270
Tiago Rodrigues <mail@tig.pt>
2023-01-02 22:55:29 +0000

    improve images local caching

--- Shared/AuxiliarViews/AwsServiceImageView.swift
+++ Shared/AuxiliarViews/AwsServiceImageView.swift
@@ -6,11 +6,12 @@
 //
 
 import SwiftUI
+import CachedAsyncImage
 
 struct AwsServiceImageView: View {
     let service: awsService
     var body: some View{
-        AsyncImage(url: URL(string: service.imageURL)) { phase in
+        CachedAsyncImage(url: URL(string: service.imageURL)) { phase in
             if let image = phase.image{
                 image
                     .resizable()
@@ -25,13 +26,22 @@ struct AwsServiceImageView: View {
                     .frame(width: 100, height: 100, alignment: .center)
             }
         }.onDrag({
-            print("Dragging name:\(service.name) id:\(service.id)")
-            let dragImage = UIImage(url: URL(string: service.imageURL))
-            let itemProvider = NSItemProvider(object: dragImage! as UIImage)
-            return itemProvider
+           print("Dragging name:\(service.name) id:\(service.id)")
+           let cache = URLCache.shared
+           //let request = URLRequest(url: URL(string: service.imageURL)!)
+           var dragImage: UIImage
+           let request = URLRequest(url: URL(string: service.imageURL)!, cachePolicy: URLRequest.CachePolicy.returnCacheDataElseLoad, timeoutInterval: 60.0)
+           if let data = cache.cachedResponse(for: request)?.data {
+              dragImage = UIImage(data:data)!
+           } else {
+              dragImage = UIImage(url: URL(string: service.imageURL))!
+           }
+           let itemProvider = NSItemProvider(object: dragImage as UIImage)
+           return itemProvider
         })
     }
 }
+
 
 extension UIImage {
    convenience init?(url: URL?) {
