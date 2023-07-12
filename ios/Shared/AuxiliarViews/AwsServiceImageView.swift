a12018dd80b4731c63c79c315c3a78862b949ac1
Tiago Rodrigues <mail@tig.pt>
2023-07-12 19:39:41 +0100

    ios - local image for faster interactions

--- ios/Shared/AuxiliarViews/AwsServiceImageView.swift
+++ ios/Shared/AuxiliarViews/AwsServiceImageView.swift
@@ -11,26 +11,38 @@ import SDWebImageSwiftUI
 struct AwsServiceImageView: View {
     let service: awsService
     var body: some View{
-       WebImage(url: URL(string: service.imageURL))
-          .resizable()
-          .indicator(.activity)
-          .scaledToFit()
-          .onDrag({
-           print("Dragging name:\(service.name) id:\(service.id)")
-           let cache = URLCache.shared
-           //let request = URLRequest(url: URL(string: service.imageURL)!)
-           var dragImage: UIImage
-           let request = URLRequest(url: URL(string: service.imageURL)!, cachePolicy: URLRequest.CachePolicy.returnCacheDataElseLoad, timeoutInterval: 60.0)
-           if let data = cache.cachedResponse(for: request)?.data {
-              dragImage = UIImage(data:data)!
-           } else {
-              dragImage = UIImage(url: URL(string: service.imageURL))!
-           }
-           let itemProvider = NSItemProvider(object: dragImage as UIImage)
-           return itemProvider
-        })
-        .frame(width: 100, height: 100, alignment: .center)
-        .cornerRadius(8)
+       Image(service.imageURL.replacingOccurrences(of: "https://static.tig.pt/awsary/logos/", with: "").replacingOccurrences(of: ".png", with: ""))
+                   .resizable()
+                   .scaledToFit()
+                   .onDrag({
+                      print("Dragging name:\(service.name) id:\(service.id)")
+                      var dragImage: UIImage
+                      dragImage = UIImage(named: service.imageURL.replacingOccurrences(of: "https://static.tig.pt/awsary/logos/", with: "").replacingOccurrences(of: ".png", with: ""))!
+                      let itemProvider = NSItemProvider(object: dragImage as UIImage)
+                      return itemProvider
+                   })
+                   .frame(width: 100, height: 100, alignment: .center)
+                   .cornerRadius(8)
+//       WebImage(url: URL(string: service.imageURL))
+//          .resizable()
+//          .indicator(.activity)
+//          .scaledToFit()
+//          .onDrag({
+//           print("Dragging name:\(service.name) id:\(service.id)")
+//           let cache = URLCache.shared
+//           //let request = URLRequest(url: URL(string: service.imageURL)!)
+//           var dragImage: UIImage
+//           let request = URLRequest(url: URL(string: service.imageURL)!, cachePolicy: URLRequest.CachePolicy.returnCacheDataElseLoad, timeoutInterval: 60.0)
+//           if let data = cache.cachedResponse(for: request)?.data {
+//              dragImage = UIImage(data:data)!
+//           } else {
+//              dragImage = UIImage(url: URL(string: service.imageURL))!
+//           }
+//           let itemProvider = NSItemProvider(object: dragImage as UIImage)
+//           return itemProvider
+//        })
+//        .frame(width: 100, height: 100, alignment: .center)
+//        .cornerRadius(8)
     }
 }
 
--- ios/Shared/MainViews/DetailsView.swift
+++ ios/Shared/MainViews/DetailsView.swift
@@ -17,7 +17,7 @@ struct DetailsView: View {
       ScrollView{
          VStack{
             HStack{
-               AwsServiceImageView(service: service).frame(maxWidth: 40, maxHeight: 40)
+               AwsServiceImageView(service: service).frame(maxWidth: 100, maxHeight: 100)
                Text(service.longName).font(Font.title)
             }
             Spacer()
