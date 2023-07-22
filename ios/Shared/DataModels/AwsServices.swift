5a101037f048e34edfc08a94e996cec5b8539fa0
Tiago Rodrigues <mail@tig.pt>
2023-07-22 09:37:02 +0100

    local json file

--- ios/Shared/DataModels/AwsServices.swift
+++ ios/Shared/DataModels/AwsServices.swift
@@ -7,37 +7,49 @@
 
 import Foundation
 
+var fm = FileManager.default
+var subUrl: URL?
+var mainUrl: URL? = Bundle.main.url(forResource: "aws_services", withExtension: "json")
+
 class AwsServices: ObservableObject {
     @Published var services = [awsService]()
     
     init() {
-        refresh()
+       refresh()
     }
     
+   func getData() {
+           do {
+               let documentDirectory = try fm.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+               subUrl = documentDirectory.appendingPathComponent("aws_services.json")
+               loadFile(mainPath: mainUrl!, subPath: subUrl!)
+           } catch {
+               print(error)
+           }
+       }
+   
+   func loadFile(mainPath: URL, subPath: URL){
+           if fm.fileExists(atPath: subPath.path){
+               decodeData(pathName: subPath)
+               
+               if services.isEmpty{
+                   decodeData(pathName: mainPath)
+               }
+               
+           }else{
+               decodeData(pathName: mainPath)
+           }
+       }
+   
+   func decodeData(pathName: URL){
+           do{
+               let jsonData = try Data(contentsOf: pathName)
+               let decoder = JSONDecoder()
+              services = try decoder.decode([awsService].self, from: jsonData)
+           } catch {}
+       }
+   
     func refresh(){
-        //        Get json from bundle instead of internet for offline debug
-        //        guard let url = Bundle.main.url(forResource: "services", withExtension: "json") else {
-        //            print("JSON file not found")
-        //            return
-        //        }
-        //        let data = try? Data(contentsOf: url)
-        //                let awsServices = try? JSONDecoder().decode([awsService].self, from: data!)
-        //                self.awsServices = awsServices!
-        //        Get json from internet
-        let url = URL(string: "https://static.tig.pt/awsary/services_app_store.json")!
-        URLSession.shared.dataTask(with: url) {(data, response, error) in
-            do {
-                if let awsServiceData = data {
-                    let decodedData = try JSONDecoder().decode([awsService].self, from: awsServiceData)
-                    DispatchQueue.main.async {
-                        self.services = decodedData
-                    }
-                } else {
-                    print("No data")
-                }
-            } catch {
-                print("Error")
-            }
-        }.resume()
+       getData()
     }
 }
