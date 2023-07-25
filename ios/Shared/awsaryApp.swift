5a43c6f852ba5634f0f93788883d18c8fb05d136
Tiago Rodrigues <mail@tig.pt>
2023-07-25 19:50:58 +0100

    remove unused comment

--- ios/Shared/awsaryApp.swift
+++ ios/Shared/awsaryApp.swift
@@ -5,10 +5,6 @@
 //  Created by Tiago Rodrigues on 15/07/2022.
 //
 
-// TODO - Make at least 50Mb of cache to fit all images, more for some spare room.
-// https://smashswift.com/how-to-change-urlsession-cache-size/
-
-
 import SwiftUI
 
 @main
