ba00f5b64eb6cd0cf8e5145017e0ff4a190fd94d
Tiago Rodrigues <mail@tig.pt>
2024-02-03 10:16:30 +0000

    Create a game view

---
+++ ios/Shared/MainViews/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  awsary
+//
+//  Created by Tiago Rodrigues on 03/02/2024.
+//
+
+import SwiftUI
+
+struct Game: View {
+    var body: some View {
+        Text("Hello, World from GAME!")
+    }
+}
+
+#Preview {
+    Game()
+}
