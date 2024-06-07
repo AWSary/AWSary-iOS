296a957a068c6f1e3d10f0bedf1864f916848b24
Tiago Rodrigues <mail@tig.pt>
2024-06-07 21:24:24 +0100

    First Screen CTA

--- ios/Shared/MainViews/ContentView.swift
+++ ios/Shared/MainViews/ContentView.swift
@@ -26,6 +26,31 @@ struct ContentView: View {
       TabView {
          NavigationStack{
             ScrollView{
+               HStack{
+                  Image("bunny-ses")
+                     .resizable()
+                     .scaledToFit()
+                     .frame(width: 100, height: 100)
+                  VStack{
+                     Text("Celebrate Bunny SES launch")
+                     Text("10% Discount Code: GSC293ZEQD").font(.footnote)
+                     Text("\nUntil 31st of June 2024").font(.footnote)
+                  }
+               }.padding(6)
+                  .background(Color(light: .white, dark: .black))
+                  .cornerRadius(8.0)
+                  .padding(.horizontal, 3)
+                  .padding(.vertical, 3)
+                  .background(Color(red:1.0, green: 0.5, blue: 0.0))
+                  .cornerRadius(8.0)
+                  .background(.pink)
+                  .cornerRadius(8.0)
+                  .onTapGesture {
+                     guard let url = URL(string: "https://bit.ly/4aT6CYx") else {
+                        return
+                     }
+                     UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                  }
                LazyVGrid(
                   columns: [GridItem(.adaptive(minimum: 100))], content: {
                      ForEach(filteredAwsServices, id: \.self){ service in
