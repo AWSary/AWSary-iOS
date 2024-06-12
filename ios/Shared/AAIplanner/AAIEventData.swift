c1668094efbfe45dd37df0b86d8e30d2e06732a9
Tiago Rodrigues <mail@tig.pt>
2024-06-12 19:56:42 +0100

    add all AAI trainings

--- ios/Shared/AAIplanner/AAIEventData.swift
+++ ios/Shared/AAIplanner/AAIEventData.swift
@@ -2,13 +2,48 @@ import SwiftUI
 import Foundation
 
 struct AAIEventData {
+   @ObservedObject var userModel = UserViewModel.shared
+   
     static let eventNames = [
-        "Architecting on AWS",
-        "Advanced Architecting on AWS",
-        "Developing on AWS",
-        "Advanced Developing on AWS"
+      "AWS Cloud Essentials for Business Leaders",
+      "AWS Cloud Essentials for Business Leaders – Financial Services",
+      "AWS Cloud Practitioner Essentials",
+      "AWS Technical Essentials",
+      "AWS Security Essentials",
+      "Architecting on AWS",
+      "AWS Cloud Financial Management for Builders",
+      "AWS Cloud for Finance Professionals",
+      "AWS Security Best Practices",
+      "AWS Security Governance at Scale",
+      "AWS Well-Architected Best Practices",
+      "Advanced AWS Well-Architected Best Practices",
+      "Big Data on AWS",
+      "Building Batch Data Analytics Solutions on AWS",
+      "Building Data Analytics Solutions Using Amazon Redshift",
+      "Building Data Lakes on AWS",
+      "Building Streaming Data Analytics on AWS",
+      "Cloud Operations on AWS",
+      "Data Warehousing on AWS",
+      "Deep Learning on AWS",
+      "Developing on AWS",
+      "Developing Serverless Solutions on AWS",
+      "DevOps Engineering on AWS",
+      "Migrating to AWS",
+      "MLOps Engineering on AWS",
+      "Planning and Designing Databases on AWS",
+      "Practical Data Science with Amazon SageMaker",
+      "Running Containers on Amazon Elastic Kubernetes Service",
+      "Security Engineering on AWS",
+      "The Machine Learning Pipeline on AWS",
+      "Video Streaming Essentials for AWS Media Services",
+      "Advanced Architecting on AWS",
+      "Advanced Developing on AWS",
+      "Architecting on AWS - Accelerator",
+      "Amazon SageMaker Studio for Data Scientists",
+      "Authoring Visual Analytics Using Amazon QuickSight",
     ]
     
+   // TODO add real data to the trainings
     static let eventSequences: [String: [(name: String, duration: Int)]] = [
         "Architecting on AWS": [("AoA - Module 0", 20), ("AoA - Module 1", 45), ("AoA - Module 2", 20)],
         "Advanced Architecting on AWS": [("AAoA - Module 0", 20), ("AAoA - Module 1", 45), ("AAoA - Module 2", 20)],
