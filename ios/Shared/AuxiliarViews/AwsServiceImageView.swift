30d1cb1524686bc0aa4006eb3950906002bfbc27
Tiago Rodrigues <mail@tig.pt>
2024-11-06 14:20:25 +0000

    fix 'windows' was deprecated in iOS 15.0

--- ios/Shared/AuxiliarViews/AwsServiceImageView.swift
+++ ios/Shared/AuxiliarViews/AwsServiceImageView.swift
@@ -83,7 +83,10 @@ extension View {
         controller.view.backgroundColor = .clear
         
         controller.view.frame = CGRect(x: 0, y: CGFloat(Int.max), width: 1, height: 1)
-        UIApplication.shared.windows.first!.rootViewController?.view.addSubview(controller.view)
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first {
+            window.rootViewController?.view.addSubview(controller.view)
+        }
         
         let size = controller.sizeThatFits(in: UIScreen.main.bounds.size)
         controller.view.bounds = CGRect(origin: .zero, size: size)
