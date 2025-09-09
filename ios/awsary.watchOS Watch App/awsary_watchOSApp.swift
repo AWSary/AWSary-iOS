68f36fbd7622e56dd38fdedb2da3314cee7943da
Tiago Rodrigues <mail@tig.pt>
2025-09-09 21:59:29 +0100

    new apple watch app and icons

---
+++ ios/awsary.watchOS Watch App/awsary_watchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  awsary_watchOSApp.swift
+//  awsary.watchOS Watch App
+//
+//  Created by Tiago Rodrigues on 09/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct awsary_watchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
